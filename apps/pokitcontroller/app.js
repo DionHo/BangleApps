af2b348c883e5d9e01ead4331bf3414e68ed4a73
Dion Hornig <dionhornig@gmail.com>
2024-03-09 11:57:20 +0100

    Established connection, but unable to receive measurement notiffications

--- apps/pokitcontroller/app.js
+++ apps/pokitcontroller/app.js
@@ -29,40 +29,52 @@ Bangle.loadWidgets();
 Bangle.drawWidgets();
 E.showMessage(/*LANG*/"Connecting...");
 
-NRF.requestDevice({ filters: [{ services: '57d3a771-267c-4394-8872-78223e92aec4' }] }).then(function(device) {
-  return device.gatt.connect();
-}).then(function(g) {
-  gatt = g;
-  return gatt.getPrimaryService("7481d2f-5781-442e-bb9a-fd4e3441dadc");
-}).then(function(service) {
-  mmservice = service
-  return mmservice.getCharacteristic("53dc9a7a-bc19-4280-b76b-002d0e23b078");
-}).then(function(characteristic) {
-  return characteristic.writeValue(0x01FF0003e8);
-}).then(function (){
-  mmservice.getCharacteristic("047d3559-8bee-423a-b229-4417fa603b90");
-}).then(function(c) {
-  c.on('characteristicvaluechanged', function(event) {
-    d = event.target.value;
-    decode(d);
+function connection_setup() {
+  E.showMessage("Scanning for Pokit Meter...");
+  NRF.requestDevice({ filters: [{timeout: 20000, services:["57d3a771-267c-4394-8872-78223e92aec4"]}]}).then(function(d) {
+    device = d;
+    E.showMessage("Found pokit");
+    console.log("Found pokit");
+    return device.gatt.connect();
+  }).then(function(ga) {
+    gatt = ga;
+    E.showMessage("Connected");
+    console.log("Connected");
+    return gatt.getPrimaryService("e7481d2f-5781-442e-bb9a-fd4e3441dadc");
+  }).then(function(service) {
+    console.log("Got primary service");
+    mmservice = service;
+    return mmservice.getCharacteristic("53dc9a7a-bc19-4280-b76b-002d0e23b078");
+  }).then(function(characteristic) {
+    console.log("Set to voltage measurement");
+    var b = new ArrayBuffer(6);
+    var v = new DataView(b);
+    v.setUint8(0,1);
+    v.setUint8(1,255);
+    v.setUint32(2,1000);
+    return characteristic.writeValue(b);
+  }).then(function (){
+    return mmservice.getCharacteristic("047d3559-8bee-423a-b229-4417fa603b90");
+  }).then(function(c) {
+    console.log("read measurement value");
+    console.log(c);
+    c.on('characteristicvaluechanged', function(event) {
+      d = event.target.value;
+      decode(d);
+    });
+    return c.startNotifications();
+  }).then(function() {
+    console.log("Done!");
+  }).catch(function(e) {
+    E.showMessage(e.toString(), "ERROR");
+    console.log(e);
+    setTimeout(connection_setup, 1000);
   });
-  return c.startNotifications();
-}).then(function() {
-  E.showMessage(/*LANG*/"Connected.");
-}).catch(function(e) {
-  E.showMessage(e.toString());
+}
+
+connection_setup();
+E.on('kill',()=>{
+  if (gatt!=undefined) gatt.disconnect();
 });
+NRF.on('disconnect', connection_setup); // restart if disconnected
 
-
-
-// .then(function(c) {
-//   c.on('characteristicvaluechanged', function(event) {
-//     d = event.target.value;
-//     decode(d);
-//   });
-//   return c.startNotifications();
-// }).then(function() {
-//   E.showMessage(/*LANG*/"Connected.");
-// }).catch(function(e) {
-//   E.showMessage(e.toString());
-// });
