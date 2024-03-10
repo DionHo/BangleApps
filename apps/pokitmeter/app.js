7da92aac283e41f606046aec9c44b5d022ba7edc
Dion Hornig <dionhornig@gmail.com>
2024-03-10 21:17:24 +0100

    Refactoring

--- apps/pokitmeter/app.js
+++ apps/pokitmeter/app.js
@@ -1,5 +1,6 @@
-var gatt;
-var mmservice;
+const mmservice_uuid             = "e7481d2f-5781-442e-bb9a-fd4e3441dadc";
+const mmservice_setmode_uuid     = "53dc9a7a-bc19-4280-b76b-002d0e23b078";
+const mmservice_measurement_uuid = "047d3559-8bee-423a-b229-4417fa603b90";
 
 var MMModeTitle = {
   0:"IDLE",
@@ -17,7 +18,11 @@ var MMModeUnit = {
   4:"A", 8:"°C"
 };
 
-function decodeAndShowMMMeasurement(d) {
+var gatt;
+var mmservice;
+
+
+function decodeMMAndShow(d) {
 //  var status = d.getUint8(0);
   var value = d.getFloat32(1,true);
   var mode = d.getUint8(5);
@@ -36,6 +41,19 @@ function decodeAndShowMMMeasurement(d) {
   g.drawString(value_str, R.x+R.w/2, R.y+R.h/2);
 }
 
+function setMMMode(mode) {
+  return mmservice.getCharacteristic(mmservice_setmode_uuid).then(function(characteristic) {
+    E.showMessage("Set to MultiMeter mode to \n'"+MMModeTitle[mode]+"'");
+    console.log("Set to MultiMeter mode to '"+MMModeTitle[mode]+"'");
+    var b = new ArrayBuffer(6);
+    var v = new DataView(b);
+    v.setUint8(0,mode);      // Set mesurement mode
+    v.setUint8(1,255);       // Set range to "autorange" (=255)
+    v.setUint32(2,100, true);// Set update interval to 100ms
+    return characteristic.writeValue(b);
+  });
+}
+
 function connection_setup() {
   E.showMessage("Scanning for Pokit Meter...");
   NRF.requestDevice({ filters: [{timeout: 20000, services:["57d3a771-267c-4394-8872-78223e92aec4"]}]}).then(function(d) {
@@ -46,35 +64,27 @@ function connection_setup() {
   }).then(function(ga) {
     gatt = ga;
     E.showMessage("Connected [2/6]");
-    return gatt.getPrimaryService("e7481d2f-5781-442e-bb9a-fd4e3441dadc");
+    return gatt.getPrimaryService(mmservice_uuid);
   }).then(function(service) {
     E.showMessage("Got primary service [3/6]");
     mmservice = service;
-    return mmservice.getCharacteristic("53dc9a7a-bc19-4280-b76b-002d0e23b078");
-  }).then(function(characteristic) {
-    E.showMessage("Set to voltage measurement [4/6]");
-    var b = new ArrayBuffer(6);
-    var v = new DataView(b);
-    v.setUint8(0,1);    // Set mesurement mode to voltage (=1) 
-    v.setUint8(1,255);  // Set range to "autorange" (=255)
-    v.setUint32(2,100, true);// Set update interval to 100ms
-    return characteristic.writeValue(b);
+    return setMMMode(1);
   }).then(function (response){
-    return mmservice.getCharacteristic("047d3559-8bee-423a-b229-4417fa603b90");
+    return mmservice.getCharacteristic(mmservice_measurement_uuid);
   }).then(function(c) {
     E.showMessage("register change-value notifier [5/6]");
     c.on('characteristicvaluechanged', function(event) {
       var d = event.target.value;
-      decodeAndShowMMMeasurement(d);
+      decodeMMAndShow(d);
     });
     return c.startNotifications();
   }).then(function() {
-    return mmservice.getCharacteristic("047d3559-8bee-423a-b229-4417fa603b90");
+    return mmservice.getCharacteristic(mmservice_measurement_uuid);
   }).then(function(c) {
     E.showMessage("read first measurement value [6/6]");
     return c.readValue();
   }).then(function(d) {
-    decodeAndShowMMMeasurement(d);
+    decodeMMAndShow(d);
   }).then(function (){
     console.log("Done!");
   }).catch(function(e) {
@@ -93,4 +103,3 @@ E.on('kill',()=>{
   console.log("Disconnected pokit!");
 });
 NRF.on('disconnect', connection_setup); // restart if disconnected
-
