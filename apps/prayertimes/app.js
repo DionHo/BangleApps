5301d8a2f15c99624dd593d7bf43f5fe2fd42608
Dion Hornig <dionhornig@gmail.com>
2024-03-17 21:28:05 +0100

    Fixup settings access

--- apps/prayertimes/app.js
+++ apps/prayertimes/app.js
@@ -1,16 +1,20 @@
 const SETTINGS_FILE = "prayertimes.settings.json";
+const adhan = require('adhan.espruino.js');
+
 let settings;
 function loadSettings() {
-  settings = require("Storage").readJSON(SETTINGS_FILE,true)||{};
+  settings = Object.assign({
+    latitude: parseFloat("52.2647"),
+    longitude: parseFloat("10.5237"),
+    calcmethod: "NorthAmerica"
+  }, require('Storage').readJSON(SETTINGS_FILE, true) || {});
 }
 loadSettings();
 
 
-const adhan = require('adhan.espruino.js');
-var today = new Date(Date.now());
-
+let today = new Date(Date.now());
 let coordinates = adhan.Coordinates(settings.latitude, settings.longitude);
-let prayerTimes = new adhan.PrayerTimes(coordinates, today, settings.calcmethod);
+let prayerTimes = new adhan.PrayerTimes(coordinates, today, adhan.CalculationMethod[settings.calcmethod]());
 
 function timestr(datetime){
   return datetime.getHours().toString().padStart(2,'0')+":"+datetime.getMinutes().toString().padStart(2,'0');
@@ -25,7 +29,7 @@ let menu = {
   "Asr": {value:timestr(prayerTimes.asr)},
   "Maghrib": {value:timestr(prayerTimes.maghrib)},
   "Isha": {value:timestr(prayerTimes.isha)},
-  "Settings": () => eval(require("Storage").read("prayertimes.settings.js"))(()=>{loadSettings();menuMain();})
+  "Settings": () => eval(require("Storage").read("prayertimes.settings.js"))(()=>{loadSettings();E.showMenu(menu);})
 };
 
 console.log(menu);
